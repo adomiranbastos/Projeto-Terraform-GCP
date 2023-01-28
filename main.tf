4a2ed87cd044784030a4bad94957f34e44ac8b84
Adomiran Bastos <adomiranbastos@gmail.com>
2023-01-28 22:06:58 +0000

    atualiza

---
+++ main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+
+   backend "gcs" {
+    bucket  = "superproterraform"
+    prefix  = "terraform/state"
+  }
+  
+}
+
+provider "google" {
+  project = "treinamento-gcp"
+  region  = "us-central1"
+  zone    = "us-central1-C"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "${var.network_name}"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "cloudbbuildterraform-instance"
+  machine_type = "f1-micro"
+  tags = ["prod"]
+
+  labels = {
+    centro_custo = "${var.centro_custo_rh}"
+  }
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+    network = "google_compute_network.vpc_network.name"
+    access_config {
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "network_name" {
+  description = "rede superpro"
+  type        = string
+  default     = "terraform-network"
+}
+
+variable "centro_custo_rh" {
+  description = "custos de rede superpro"
+  type        = string
+  default     = "rh"
+}
+
+
